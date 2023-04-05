fix variance calculation

ThompsonSampling/Bandit.py
@@ -54,7 +54,7 @@ class GaussianThompson(Bandit):
 
     def sample(self):
         """ return a value from the posterior normal distribution """
-        return np.random.normal(loc=self.mu_0, scale=1/self.tau_0)
+        return np.random.normal(loc=self.mu_0, scale=1/np.sqrt(self.tau_0))
 
     def update(self, R):
         """ update this arm after it has returned reward value 'R' """

ThompsonSampling/main.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     x = np.linspace(0.0, 16.0, 200)
     # set the random seed to produce a recreatable graph
     
-    seed = 15
+    seed = 42
     random.seed(seed)
     np.random.seed(seed)
 