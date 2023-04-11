2766e6439d5d0dba6e6ff32d11dd284c4185147d
Jordan Palmer <jordanpalmer@Jordans-MacBook-Pro-2.local>
2023-04-11 15:47:38 +0100

    now can take a simulated dataset

--- ThompsonSampling/simulations/simulation.py
+++ ThompsonSampling/simulations/simulation.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 from IPython.core.pylabtools import figsize
 import utils
 from Bandit import *
+import change_point.change_point_rupture as changepoint
 
 
 class ArmTester:
@@ -18,7 +19,7 @@ class ArmTester:
     modified from: https://github.com/WhatIThinkAbout/BabyRobot/blob/master/Multi_Armed_Bandits/PowerarmSystem.py
     """
 
-    def __init__(self, bandit=GaussianThompson, arm_order: list = [], multiplier=2, **kwargs):
+    def __init__(self, bandit=GaussianThompson, arm_values: list = [], multiplier=2, **kwargs):
 
         # some quantities to track
         self.arm_stats = None
@@ -29,14 +30,14 @@ class ArmTester:
         self.number_of_steps = None
 
         # create supplied arm type with a mean value defined by arm order
-        self.arms = [bandit((q * multiplier), **kwargs) for q in arm_order]
+        self.arms = [bandit((q * multiplier), **kwargs) for q in arm_values]
 
         # set the number of arms equal to the number created
         self.number_of_arms = len(self.arms)
 
-        # the index of the best arm is the last in the arm_order list
+        # the index of the best arm is the last in the arm_values list
         # - this is a one-based value so convert to zero-based
-        self.optimal_arm_index = (arm_order[-1] - 1)
+        self.optimal_arm_index = (arm_values[-1] - 1)
 
         # by default a arm tester records 2 bits of information over a run
         self.number_of_stats = kwargs.pop('number_of_stats', 2)
@@ -125,9 +126,28 @@ class ArmTester:
         arm_index = utils.random_argmax([arm.sample() for arm in self.arms])
         return arm_index
 
-    def run(self, number_of_steps, maximum_total_reward=float('inf')):
+    def run(self,
+            number_of_steps,
+            data: list = [],
+            maximum_total_reward=float('inf'),
+            change_point_method: str = None,
+            change_point_burn: int = 10,
+            adjust_for_change_point: bool = False,
+            change_point_window: int = None
+            ):
         """ perform a single run, over the set of arms, 
             for the defined number of steps """
+
+        # check change point is a valid method
+        change_point_methods = {'window': changepoint.window, 'Pelt': changepoint.pelt}
+        assert (change_point_method in change_point_methods.keys()) or (change_point_method is None)
+        print(data)
+        # has data been passed in?
+        if len(data) != 0:
+            number_of_steps = len(data)
+
+        # store rewards for change point detection
+        arm_observations = [[] for _ in range(len(self.arms))]
 
         # reset the run counters
         self.initialize_run(number_of_steps)
@@ -138,12 +158,35 @@ class ArmTester:
             # get information about all arms at the start of the time step
             self.arm_stats[t] = self.get_arm_stats(t)
 
-            # select a arm
+            # select an arm
             arm_index = self.select_arm(t)
+            print(data)
+            # if no data, simulate next observation, else take the next data point
+            if len(data) == 0:
+                # simulate_observation from the chosen arm and update its mean reward value
+                reward = self.simulate_observation(arm_index)
+            else:
+                reward = data[t]
 
-            # simulate_observation from the chosen arm and update its mean reward value
-            val = self.simulate_observation(arm_index)
-            self.update(arm_index, val)
+            self.update(arm_index, reward)
+
+            # CHANGE POINT DETECTION
+            if change_point_method is not None:  # check for change point
+                if (change_point_window is None) or (len(arm_observations[arm_index]) < change_point_window):
+                    # append to unconstrained list if no change point window
+                    # store reward for change point detection
+                    arm_observations[arm_index].append(reward)
+                else:
+                    # if there is a change point window, only store that many rewards
+                    arm_observations[arm_index].pop(0)
+                    arm_observations[arm_index].append(reward)
+
+                # change point detection - only when we have seen at least change_point_burn amount
+                if len(arm_observations[arm_index]) >= change_point_burn:
+                    result, distance, prob = change_point_methods[change_point_method](self.arms[arm_index],
+                                                                                       arm_observations[arm_index])
+                    if (result is not None) and adjust_for_change_point:
+                        self.arms[arm_index].mu_0 += distance
 
             # test if the accumulated total reward is greater than the maximum
             if self.total_reward > maximum_total_reward:
@@ -155,13 +198,8 @@ class ArmTester:
         # get the stats for each arm at the end of the run        
         self.arm_stats[t + 1] = self.get_arm_stats(t + 1)
         # x = np.linspace(0, 20, 100)
-        # self.arms[0].plot_arms(x, self.arms, [((q * self.multiplier) + 2) for q in self.arm_order])
+        # self.arms[0].plot_arms(x, self.arms, [((q * self.multiplier) + 2) for q in self.arm_values])
         return self.total_steps, self.total_reward
-
-
-def update_mean(current_mean, new_value, n):
-    """ calculate the new mean from the previous mean and the new value """
-    return (1 - 1.0 / n) * current_mean + (1.0 / n) * new_value
 
 
 class ArmExperiment:
@@ -197,10 +235,8 @@ class ArmExperiment:
         self.arm_percentages = np.zeros(self.number_of_arms)
         self.estimates = np.zeros(shape=(self.number_of_steps + 1, self.number_of_arms))
         self.number_of_trials = np.zeros(shape=(self.number_of_steps + 1, self.number_of_arms))
-
         # the cumulative total reward per timestep
         self.cumulative_reward_per_timestep = np.zeros(shape=self.number_of_steps)
-
         # the actual reward obtained at each timestep
         self.reward_per_timestep = np.zeros(shape=self.number_of_steps)
 
@@ -251,15 +287,19 @@ class ArmExperiment:
 
         return (1 - 1.0 / n) * current_mean + (1.0 / n) * new_array
 
+    def update_mean(self, current_mean, new_value, n):
+        """ calculate the new mean from the previous mean and the new value """
+        return (1 - 1.0 / n) * current_mean + (1.0 / n) * new_value
+
     def record_test_stats(self, n):
         """ update the mean value for each statistic being tracked over a run """
 
         # calculate the new means from the old means and the new value
         tester = self.arm_tester
-        self.mean_total_reward = update_mean(self.mean_total_reward, tester.get_mean_reward(), n)
-        self.optimal_selected = update_mean(self.optimal_selected, tester.get_optimal_arm_percentage(), n)
-        self.arm_percentages = update_mean(self.arm_percentages, tester.get_arm_percentages(), n)
-        self.mean_time_steps = update_mean(self.mean_time_steps, tester.get_time_steps(), n)
+        self.mean_total_reward = self.update_mean(self.mean_total_reward, tester.get_mean_reward(), n)
+        self.optimal_selected = self.update_mean(self.optimal_selected, tester.get_optimal_arm_percentage(), n)
+        self.arm_percentages = self.update_mean(self.arm_percentages, tester.get_arm_percentages(), n)
+        self.mean_time_steps = self.update_mean(self.mean_time_steps, tester.get_time_steps(), n)
 
         self.cumulative_reward_per_timestep = self.update_mean_array(self.cumulative_reward_per_timestep,
                                                                      tester.get_total_reward_per_timestep(), n)
@@ -273,49 +313,67 @@ class ArmExperiment:
                                                               tester.get_reward_per_timestep(), n)
             self.number_of_trials = self.update_mean_array(self.number_of_trials, tester.get_number_of_trials(), n)
 
-    def run(self):
+    def run(self,
+            data: list = [],
+            maximum_total_reward=float('inf'),
+            change_point_method: str = None,
+            change_point_burn: int = 10,
+            adjust_for_change_point: bool = False,
+            change_point_window: int = None):
         """ repeat the test over a set of arms for the specified number of trials """
-
         # do the specified number of runs for a single test
         self.initialize_run()
         for n in range(1, self.number_of_tests + 1):
             # do one run of the test
-            self.arm_tester.run(self.number_of_steps, self.maximum_total_reward)
+            self.arm_tester.run(self.number_of_steps,
+                                data=data,
+                                maximum_total_reward=self.maximum_total_reward,
+                                change_point_method=change_point_method,
+                                change_point_burn=change_point_burn,
+                                adjust_for_change_point=adjust_for_change_point,
+                                change_point_window=change_point_window
+                                )
             self.record_test_stats(n)
 
 
 if __name__ == '__main__':
-    ArmTester(bandit=GaussianThompson, arm_order=[2, 1, 3, 5, 4], multiplier=2).run(100)
-    ArmTester(bandit=BernoulliThompson, arm_order=[0.5, 0.1, 0.3, 0.2, 0.8], multiplier=1).run(100)
+    """
+    Example use cases
+    """
+    # add change point
+    # input means not arm order
+    ArmTester(bandit=GaussianThompson, arm_values=[2, 1, 3, 5, 4], multiplier=2).run(100, change_point_method='window',
+                                                                                     change_point_window=30)
+    ArmTester(bandit=BernoulliThompson, arm_values=[0.5, 0.1, 0.3, 0.2, 0.8], multiplier=1).run(100)
 
-    gaussian_thompson = ArmTester(bandit=GaussianThompson, arm_order=[2, 1, 3, 5, 4], multiplier=2)
-    bernoulli_thompson = ArmTester(bandit=BernoulliThompson, arm_order=[0.5, 0.1, 0.3, 0.2, 0.8], multiplier=1)
+    gaussian_thompson = ArmTester(bandit=GaussianThompson, arm_values=[2, 1, 3, 5, 4], multiplier=2)
+    bernoulli_thompson = ArmTester(bandit=BernoulliThompson, arm_values=[0.5, 0.1, 0.3, 0.2, 0.8], multiplier=1)
 
     # experiments
     number_of_steps = 100
     experiment_gaussian = ArmExperiment(arm_tester=gaussian_thompson, number_of_steps=number_of_steps)
     experiment_gaussian.run()
-
-    # REGRET
-    cumulative_optimal_reward = [r * 10 for r in range(1, number_of_steps + 1)]
-    regret = cumulative_optimal_reward - experiment_gaussian.get_cumulative_reward_per_timestep()
-
-    fig = plt.figure(figsize=(20, 6))
-    plt.suptitle(f'Epsilon Greedy Regret', fontsize=20, fontweight='bold')
-
-    plt.subplot(1, 2, 1)
-    plt.plot(experiment_gaussian.get_cumulative_reward_per_timestep(), label="Actual")
-    plt.plot(cumulative_optimal_reward, label="Optimal")
-    plt.plot(regret, label="Regret")
-    plt.legend()
-    plt.title('Cumulative Reward vs Time', fontsize=15)
-    plt.xlabel('Time Steps')
-    plt.ylabel('Total Reward')
-
-    plt.subplot(1, 2, 2)
-    plt.title('Regret vs Time', fontsize=15)
-    plt.plot(regret)
-    plt.xlabel('Time Steps')
-    plt.ylabel('Regret')
-
-    plt.show()
+    #
+    # # REGRET
+    # cumulative_optimal_reward = [r * 10 for r in range(1, number_of_steps + 1)]
+    # regret = cumulative_optimal_reward - experiment_gaussian.get_cumulative_reward_per_timestep()
+    #
+    # fig = plt.figure(figsize=(20, 6))
+    # plt.suptitle(f'Epsilon Greedy Regret', fontsize=20, fontweight='bold')
+    #
+    # plt.subplot(1, 2, 1)
+    # plt.plot(experiment_gaussian.get_cumulative_reward_per_timestep(), label="Actual")
+    # plt.plot(cumulative_optimal_reward, label="Optimal")
+    # plt.plot(regret, label="Regret")
+    # plt.legend()
+    # plt.title('Cumulative Reward vs Time', fontsize=15)
+    # plt.xlabel('Time Steps')
+    # plt.ylabel('Total Reward')
+    #
+    # plt.subplot(1, 2, 2)
+    # plt.title('Regret vs Time', fontsize=15)
+    # plt.plot(regret)
+    # plt.xlabel('Time Steps')
+    # plt.ylabel('Regret')
+    #
+    # plt.show()
