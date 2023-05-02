98c2466bb5c6d762d1107cf78ccf2178c4eb5f88
Jordan Palmer <jordanpalmer@Jordans-MacBook-Pro-2.local>
2023-05-02 17:07:55 +0100

    remove prints

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,2 +1,8 @@
-def test_random_argmax():
+import pytest
+from src.utils import *
+@pytest.mark.parametrize("values, expected_result", [
+    ([], False),
+    ([], 2),
+])
+def test_random_argmax(values, expected_result):
     assert True
